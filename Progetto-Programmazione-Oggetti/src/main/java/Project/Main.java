180d49e6420bc5750368b8f399c7f216c3e7eb65
Riccardo De Ritis <s1094657@studenti.univpm.it>
2020-12-09 17:13:40 +0100

    Progetto senza filtri

---
+++ Progetto-Programmazione-Oggetti/src/main/java/Project/Main.java
@@ -0,0 +1,41 @@
+package Project;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+import Project.model.City;
+import Project.service.ParseJsonClass;
+
+public class Main {
+	
+	public static Scanner input = new Scanner(System.in);
+	private static boolean ok=true;
+	
+	public static void main(String[] args) {
+		
+		Vector<City> cityArray = new Vector<City>();
+		City city = new City();
+		ParseJsonClass parse;
+		
+		final String ApiKey="7d93f19f21077353e39f87032051beae";
+		
+		do {
+			System.out.print("Inserisci il paese : ");
+			String paese=input.nextLine();
+			String url = "https://api.openweathermap.org/data/2.5/weather?q="+paese+"&units=metric&appid="+ApiKey;
+			parse = new ParseJsonClass();
+			String data=parse.ApiData(url);
+			cityArray.add(parse.Parse(data, city));
+			city.CityViewToday(cityArray);
+			char scelta = input.next().charAt(0);
+			if(scelta=='n')
+				setOk(false);
+		}while(ok==true);
+	}
+
+	public static boolean setOk(boolean ok) {
+		Main.ok = ok;
+		return ok;
+	}
+	
+}

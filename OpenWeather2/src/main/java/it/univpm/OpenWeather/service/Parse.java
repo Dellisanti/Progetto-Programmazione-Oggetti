7e4f5cebc2434ec4fa8b6f095f6767af9669aa21
fdellisanti <S1095745@studenti.univpm.it>
2021-01-08 18:47:44 +0100

    update parsing

--- OpenWeather2/src/main/java/it/univpm/OpenWeather/service/Parse.java
+++ OpenWeather2/src/main/java/it/univpm/OpenWeather/service/Parse.java
@@ -41,7 +41,7 @@ public class Parse {
 		}
 		JSONObject SRiseSSet= (JSONObject) obj.get("sys");
 		city.setName(paese);
-		long sunrise = (Long)SRiseSSet.get("sunrise") - 3600;
+		long sunrise = (Long)SRiseSSet.get("sunrise");
 		String dateSunrise = sdf.format(new java.util.Date(sunrise*1000));
 		city.setSunrise(dateSunrise);
 		long sunset = (Long)SRiseSSet.get("sunset");
